f7c60cb9dfa6392ac8795d46bf67d32d5e61a52e
Erin Polhemus <espolhemus@gmail.com>
2023-12-14 20:50:46 -0600

    initial attempts at creating models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,41 @@
 const User = require('./User');
-const Project = require('./Project');
+const Book = require('./Book');
+const Collection = require('./Collection');
+const Review = require('./Review');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
+// We should decided whether we want to allow a user to have multiple 'collections'
+
+// One-to-one relationship
+User.hasOne(Collection, {
+  foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+// One-to-one relationhship
+Collection.belongsTo(User, {
+  foreignKey: 'userId'
 });
 
-module.exports = { User, Project };
+// One-to-many relationship
+User.hasMany(Review, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+// Many-to-one relationship
+Review.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
+// One-to-many relationship
+Collection.hasMany(Book, {
+  foreignKey: 'bookId',
+  onDelete: 'CASCADE'
+});
+
+// Many-to-one relationship
+Review.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
+module.exports = { User, Book, Collection, Review };
