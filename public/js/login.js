5923945fafd5e5e6e4202ef7bb0a3c9ce63f0eb3
Austin Oler <oleraustin@gmail.com>
2023-12-14 20:45:47 -0500

    added functionality for unique email upon sign up

--- public/js/login.js
+++ public/js/login.js
@@ -30,6 +30,15 @@ const signupFormHandler = async(event) => {
     const password = document.querySelector('#password-signup').value.trim();
 
     if (name && email && password) {
+        // Check if the email is already registered
+        const emailExists = await checkEmailExists(email);
+
+        if (emailExists) {
+            alert('This email is already registered. Please use a different email.');
+            return;
+        }
+
+        // If the email is unique, proceed with signup
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({ name, email, password }),
@@ -41,6 +50,17 @@ const signupFormHandler = async(event) => {
         } else {
             alert(response.statusText);
         }
+    }
+};
+
+const checkEmailExists = async(email) => {
+    try {
+        const response = await fetch(`/api/users/email/${email}`);
+        const data = await response.json();
+        return data.exists;
+    } catch (error) {
+        console.error('Error checking email existence:', error);
+        return false; // Assume email does not exist in case of an error
     }
 };
 
