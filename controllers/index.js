ff85df35fc6616e0f94acb10a1387f9c96bd25be
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-12-14 19:46:10 -0500

    fixed an error in routes

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,6 @@ const homeRoutes = require('./homeRoutes');
 const searchRoutes = require('./searchRoutes');
 const freeBookRoutes = require('./freeBookRoutes');
 const bookRoutes = require('./bookRoutes');
-const collectionRoutes = require('./collectionRoutes');
 
 
 router.use('/', homeRoutes);
@@ -13,7 +12,5 @@ router.use('/api', apiRoutes);
 router.use('/search', searchRoutes);
 router.use('/freebooks', freeBookRoutes);
 router.use('/books', bookRoutes);
-router.use('/collection', collectionRoutes);
-
 
 module.exports = router;
