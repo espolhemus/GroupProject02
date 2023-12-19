6fc52c6c89f77f0af28548969569063410f8316e
Nainoa Dinson <nd808@msn.com>
2023-12-18 16:06:18 -1000

    commit

--- public/js/display-search.js
+++ public/js/display-search.js
@@ -1,6 +1,6 @@
 // Search books using title input select
-function searchBooks() {
-  var apiKey=
+function searchBooks(searchInputValue, searchTypeValue) {
+  var apiKey= 'AIzaSyCn_GY-e0YI-qPTbmDGhQevrirQqWuLkVM';
   apiURL= `https://www.googleapis.com/books/v1/volumes?q=${searchInputValue}+${searchTypeValue}&key=${apiKey}`
   fetch(apiURL)
     .then(response => response.json())
@@ -11,6 +11,7 @@ function searchBooks() {
   console.error("Error:", error);
   });
 
+
 }
 
 // Display book search results to page
@@ -18,16 +19,18 @@ function displaySearchResults(books) {
   var resultsContainer = document.getElementById('output-list');
   resultsContainer.innerHTML = "";
 
-  books.forEeach(book => {
+  const [volumeInfo]=books;
+
+  books.forEeach(volumeInfo => {
     var bookCard = document.createElement("div");
       bookCard.className= "card";
       bookCard.innerHTML = `
-        <img src="${book.smallThumbnail}" class="" alt="${book.title}">
+        <img src="${volumeInfo.image.Links.smallThumbnail}" class="" alt="${volumeInfo.title}">
         <div class="card-body">
-          <h4 class="card-title">"${book.title}"</h4>
-          <h5 class="card-subtitle">${book.subtitle}</h5>
-          <p>By: ${book.authors}</p>
-          <a href="${book.infoLink}" class="text-blue-400 hover:underline">More Info</a><br>
+          <h4 class="card-title">"${volumeInfo.title}"</h4>
+          <h5 class="card-subtitle">${volumeInfo.subtitle}</h5>
+          <p>By: ${volumeInfo.authors}</p>
+          <a href="${infoLink}" class="text-blue-400 hover:underline">More Info</a><br>
           <button id="save-read" class="btn w-[150px] text-sm text-white bg-indigo-800 hover:bg-indigo-900 rounded">Save to Read</button>
           <button id="add-reading" class="btn w-[150px] text-sm text-white bg-indigo-800 hover:bg-indigo-900 rounded">Add to Reading List</button>
         <div>
@@ -49,7 +52,7 @@ function handleFormSubmit(event) {
     return;
   }
 
-  searchApi(searchInputVal, searchTypeValue);
+  searchBooks(searchInputValue, searchTypeValue);
 }
 
  // Event listener for search form submit
