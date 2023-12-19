176889b8308d7c04601ba65605b8782b889ff2fa
Austin Oler <oleraustin@gmail.com>
2023-12-18 21:01:07 -0500

    updated freeBooksRoutes.js

--- controllers/freeBookRoutes.js
+++ controllers/freeBookRoutes.js
@@ -1,23 +1,25 @@
+// freeBookRoutes.js
 const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const fetch = require('node-fetch');
 
-router.get('/', async (req, res) => {
+router.get('/', async(req, res) => {
+    const { genre } = req.query;
+
     try {
-      res.render('books', {
-        logged_in: req.session.logged_in,
-        user_id: req.session.user_id,
-        name: req.session.name
+        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&filter=free-ebooks`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
 
-      })
+        res.render('books', {
+            logged_in: req.session.logged_in,
+            user_id: req.session.user_id,
+            name: req.session.name,
+            books: data.items || [], // Pass the books data to the template
+        });
     } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    };
+        console.error('Error fetching books:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
-
-
-
-
 
 module.exports = router;
