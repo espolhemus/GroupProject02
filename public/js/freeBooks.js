925b6061d6bc028e4ca7ff6619156d842e8d6868
Austin Oler <oleraustin@gmail.com>
2023-12-19 22:29:37 -0500

    rendered results

--- public/js/freeBooks.js
+++ public/js/freeBooks.js
@@ -1,98 +1,42 @@
-/* // Search free books using title input select
+// freeBooks.js
 function searchFreeBooks() {
-    console.log("Search Free Books");
-    var apiKey = 'AIzaSyAZgRIh9j8BsCvGEJ5fYneL343gD0Qwuq0';
-    var genreInputValue = document.querySelector('#genre-input').value;
+  var apiKey = 'AIzaSyAZgRIh9j8BsCvGEJ5fYneL343gD0Qwuq0'; 
+  var selectedGenres = document.querySelectorAll('input[name="genre"]:checked');
+  var genreValues = Array.from(selectedGenres).map(genre => genre.value).join(',');
 
-    var apiURL = `/books?genre=${genreInputValue}&key=${apiKey}`;
+  var apiURL = `/books?genre=${genreValues}&key=${apiKey}`;
 
-    fetch(apiURL)
-        .then(response => response.json())
-        .then(responseData => {
-            displayFreeBooksResults(responseData.items);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+  fetch(apiURL)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json(); // Use json() instead of text()
+      })
+      .then(parsedResponse => {
+          console.log('Parsed Response:', parsedResponse);
+          renderBooks(parsedResponse.books);
+      })
+      .catch(error => {
+          console.error('Error fetching books:', error);
+      });
 }
 
-// Display free book search results to page
-function displayFreeBooksResults(books) {
-    var resultsContainer = document.getElementById('freebooks-container');
-    resultsContainer.innerHTML = "";
+function renderBooks(books) {
+  const template = Handlebars.compile(document.getElementById('books-template').innerHTML);
+  const freeBooksContainer = document.getElementById('freebooks-container');
 
-    books.forEach(book => {
-        var bookCard = document.createElement("div");
-        bookCard.className = "card";
-        bookCard.innerHTML = `
-        <img src="${book.volumeInfo.imageLinks.smallThumbnail}" class="" alt="${book.volumeInfo.title}">
-        <div class="card-body">
-          <h4 class="card-title">${book.volumeInfo.title}</h4>
-          <h5 class="card-subtitle">${book.volumeInfo.subtitle}</h5>
-          <p>By: ${book.volumeInfo.authors.join(', ')}</p>
-          <a href="${book.volumeInfo.infoLink}" class="text-blue-400 hover:underline">More Info</a><br>
-          <button id="save-read" class="btn w-[150px] text-sm text-white bg-indigo-800 hover:bg-indigo-900 rounded">Save to Read</button>
-          <button id="add-reading" class="btn w-[150px] text-sm text-white bg-indigo-800 hover:bg-indigo-900 rounded">Add to Reading List</button>
-        <div>
-      `;
-        resultsContainer.appendChild(bookCard);
-    });
+  freeBooksContainer.innerHTML = template({ books });
 }
 
-const searchFormEl = document.querySelector('#search-form');
+const genreFormEl = document.querySelector('#genre-form');
 
-function handleFormSubmit(event) {
-    event.preventDefault();
-    searchFreeBooks();
+function handleGenreFormSubmit(event) {
+  event.preventDefault();
+  searchFreeBooks();
 }
 
-// Event listener for search form submit
-searchFormEl.addEventListener('submit', handleFormSubmit); */
+// Event listener for genre form submit
+genreFormEl.addEventListener('submit', handleGenreFormSubmit);
 
-function searchFreeBooks() {
-    var apiKey = 'AIzaSyAZgRIh9j8BsCvGEJ5fYneL343gD0Qwuq0'; 
-    var selectedGenres = document.querySelectorAll('input[name="genre"]:checked');
-    var genreValues = Array.from(selectedGenres).map(genre => genre.value).join(',');
-  
-    var apiURL = `/books?genre=${genreValues}&key=${apiKey}`;
-  
-    fetch(apiURL)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.text(); // Use text() instead of json()
-  })
-  // .then(responseText => {
-  //   console.log('Response Headers:', responseText.headers); // Log headers for additional information
-  //   console.log('Response Body:', responseText); // Log the entire response body
-
-  //   try {
-  //     const responseData = JSON.parse(responseText);
-  //     console.log('API Response:', responseData);
-  //     if (Array.isArray(responseData.items)) {
-  //       displayFreeBooksResults(responseData.items);
-  //     } else {
-  //       console.error('Invalid response structure:', responseData);
-  //     }
-  //   } catch (parseError) {
-  //     console.error('Error parsing JSON:', parseError);
-  //   }
-  // })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
-  }
-  
-  const genreFormEl = document.querySelector('#genre-form');
-  
-  function handleGenreFormSubmit(event) {
-    event.preventDefault();
-    searchFreeBooks();
-  }
-  
-  // Event listener for genre form submit
-  genreFormEl.addEventListener('submit', handleGenreFormSubmit);
-  
   
